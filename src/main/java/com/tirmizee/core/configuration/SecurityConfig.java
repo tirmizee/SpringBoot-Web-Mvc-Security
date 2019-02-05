36f42b7e92f7ef704406b907c8be237f2b56802f
GDLPratya <pratya.khaday@gmail.com>
2019-02-05 22:32:37 +0700

    refactor code

--- src/main/java/com/tirmizee/core/configuration/SecurityConfig.java
+++ src/main/java/com/tirmizee/core/configuration/SecurityConfig.java
@@ -1,10 +1,14 @@
 package com.tirmizee.core.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 
 /**
@@ -13,11 +17,17 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
  */
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig  extends WebSecurityConfigurerAdapter {
 
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder(20);
+	}
+	
 	@Override
 	public void configure(WebSecurity web) throws Exception {
-		 web.ignoring().antMatchers("/resources/**").anyRequest();
+		 web.ignoring().antMatchers("/resources/**");
 	}
 
 	@Override
@@ -25,9 +35,10 @@ public class SecurityConfig  extends WebSecurityConfigurerAdapter {
 		http
 			.authorizeRequests()
 				.antMatchers("/").permitAll()
-				.anyRequest().authenticated();
+				.anyRequest().authenticated()
+				.and()
+			.formLogin()
+				.loginPage("/login").permitAll();
 	}
-	
-	
 
 }
