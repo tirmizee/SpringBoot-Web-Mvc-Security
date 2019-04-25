ca42ff0957014f80973593215262b3cc2cf62e94
GDLPratya <pratya.khaday@gmail.com>
2019-04-25 23:55:19 +0700

    update readme.md

---
+++ src/main/java/com/tirmizee/backend/api/village/ApiVillageController.java
@@ -0,0 +1,12 @@
+package com.tirmizee.backend.api.village;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/village")
+public class ApiVillageController {
+
+	
+	
+}
